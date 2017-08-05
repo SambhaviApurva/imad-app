61944c9e3e2c572223d64d6b6625cbb100c7381d
SambhaviApurva <30663347+SambhaviApurva@users.noreply.github.com>
2017-08-05 19:19:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,11 @@ element.innerHTML = "hello idiot";
 // making image move
  
 var img=document.getElementById("dino");
+var marginLeft=0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + "px";
+}
 img.onclick = function() {
-    img.style.marginLeft = "100px";
+    var interval = setInterval(moveRight,100);
 };
