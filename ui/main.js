17653d62006924c89aebf331f5fb588685234064
SambhaviApurva <30663347+SambhaviApurva@users.noreply.github.com>
2017-08-05 19:09:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 // change the text
 var element=document.getElementById("main-text");
 element.innerHTML = "hello idiot";
+
+// making image move
+ 
+var img=document.getElementById("dino");
+img.onclick = function() {
+    img.style.marginLeft = "100px";
+};
