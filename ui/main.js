eeb78d939dc0344f02db54fa648525d986dd3c66
SambhaviApurva <30663347+SambhaviApurva@users.noreply.github.com>
2017-08-06 19:19:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,19 @@ button.onclick = function()
     request.open('GET','http://gorthiuma.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit = document.getElementById('submit_btn');
+submiti.onclick = function(){
+    //make a request to the server 
+    
+    // capture a list of names and render it as the list
+    var names = ['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+= '<li' + names[i] + '</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTMl = list;
+};
