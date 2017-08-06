49186aeffd943eb65cf7c9f80a998d87fc60fcb1
SambhaviApurva <30663347+SambhaviApurva@users.noreply.github.com>
2017-08-06 18:29:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,24 @@
 //counter
 var button=document.getElementById('counter');
-var counter=0;
 
 button.onclick = function()
 {
     // make a request to catch endpoint
-    
+    var request=new XMLHttpRequest();
     // capture a request and store it in variable 
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status == 200){
+                var counter = request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
     
+        //not yet done
+    };
     //render the variable in the correct span
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML = counter.toString();
-}
+    response.open('GET','http://gorthiuma.imad.hasura-app.io/counter',true);
+    response.send(null);
+};
