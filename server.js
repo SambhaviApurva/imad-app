4145579d53764a3247b9a76f596f11eaf3a0b4c9
SambhaviApurva <30663347+SambhaviApurva@users.noreply.github.com>
2017-08-11 21:26:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,11 +122,23 @@ app.get('/submit-name',function(req,res){ // submit-name?name=xxxxx
     
 });
 
-app.get('/:articleName',function(req , res){
+app.get('/articles/:articleName',function(req , res){
     //articleName == article-one
     //Articles[articleName] == {} content object for article-one
-    var articleName=req.params.articleName;
-    res.send(createtemplate(articles[articleName]));
+    pool.query("SELECT * FROM article WHERE title =  "+req.params.articleName,function(err , result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else {
+           if(result.rows.length === 0){
+               res.status(404).send('article not found');
+           }
+           else{
+               var articleData = result.rows[0];
+                res.send(createtemplate(articleData));
+               
+           }
+       }
+    });
 });
 
 
