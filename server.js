271fa42080112262c8385102fb2421b705afe4bb
SambhaviApurva <30663347+SambhaviApurva@users.noreply.github.com>
2017-08-11 22:29:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,41 +14,6 @@ var config={
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-  'article-one' : {
-    title: 'Article one | Sambhavi Apurva',
-    heading: 'Article one',
-    date: '4th august , 2017',
-    content: 
-         `<p>
-                    this are the contents of 1st article... and it is going to be displayed in my web app.this are the contents of 1st article... and it is going to be displayed in my web app.this are the contents of 1st article... and it is going to be displayed in my web app.
-                </p>
-                <p>
-                    this are the contents of 1st article... and it is going to be displayed in my web app.this are the contents of 1st article... and it is going to be displayed in my web app.this are the contents of 1st article... and it is going to be displayed in my web app.
-                </p>
-                <p>
-                    this are the contents of 1st article... and it is going to be displayed in my web app.this are the contents of 1st article... and it is going to be displayed in my web app.this are the contents of 1st article... and it is going to be displayed in my web app.
-                </p>`
- },
-  'article-two' : {
-     title: 'Article two | Sambhavi Apurva',
-    heading: 'Article two',
-    date: '14th august , 2017',
-    content: 
-        `<p>
-            this are the contents of 2nd article... and it is going to be displayed in my web app.
-        </p>`
-},
-  'article-three' : {
-     title: 'Article three | Sambhavi Apurva',
-    heading: 'Article three',
-    date: '24th august , 2017',
-    content: 
-         `<p>
-            this are the contents of 3rd article... and it is going to be displayed in my web app.
-        </p>`
-}
-};
 function createtemplate(data) { 
         var title=data.title;
         var heading=data.heading;
